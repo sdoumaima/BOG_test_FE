244559540956eafbffb47233881afc0ec9f6b4c5
oumaimasaidii <oumaima.saidi@enicar.ucar.tn>
2023-09-23 10:16:39 +0100

    BOG-1[NavBar Modifications]

--- src/components/global/NavbarComponent.vue
+++ src/components/global/NavbarComponent.vue
@@ -11,10 +11,17 @@
 
     <ul class="flex lg:mx-auto lg:flex lg:items-center lg:w-auto space-x-6">
       <li>
+        <router-link to="/" class="text-sm text-gray-400 hover:text-gray-500">
+          Home
+        </router-link>
+      </li>
+
+      <li>
         <router-link
           to="/users"
           class="text-sm text-gray-400 hover:text-gray-500"
         >
+          Users
         </router-link>
       </li>
 
@@ -23,6 +30,7 @@
           to="/posts"
           class="text-sm text-gray-400 hover:text-gray-500"
         >
+          Posts
         </router-link>
       </li>
     </ul>
