fa0dbe9d158b08c39fdbe157b1276896c9737323
oumaimasaidii <oumaima.saidi@enicar.ucar.tn>
2023-09-25 11:04:44 +0100

    last commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="font-bold text-4xl mb-5">Welcome</h1>
+    <p class="text-xl text-gray-600 text-center">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique,
+      ratione, voluptas tenetur vero iste necessitatibus eos distinctio
+      consequatur ex aut nihil. Hic placeat beatae nihil veniam ea,
+      necessitatibus accusamus et?
+    </p>
+
+    <p class="text-xl text-gray-600 text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita animi
+      eveniet omnis esse, magni ex eius nihil doloremque magnam! Eum ratione
+      accusamus illo amet error nihil iste praesentium obcaecati placeat.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style scoped>
+/* Styles spécifiques pour ce composant */
+
+/* Style pour le titre "Welcome" */
+h1 {
+  color: #333;
+  text-align: center;
+}
+</style>
